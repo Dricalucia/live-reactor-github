c8a0a26ca3b12d439201d6850f3dd04e6a7e96ee
Adriana Lúcia <108764670+Dricalucia@users.noreply.github.com>
2024-02-21 22:31:37 -0300

    minigamer: pedra, tesoura, papel

--- app.py
+++ app.py
@@ -1,11 +1,57 @@
-#-----------------------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See LICENSE in the project root for license information.
-#-----------------------------------------------------------------------------------------
+# importar o modulo random
+import random
 
-from flask import Flask
-app = Flask(__name__)
+options = ["pedra", "papel", "tesoura"] # lista de opções
+score = 0
+rounds_played = 0
 
-@app.route("/")
-def hello():
-    return app.send_static_file("index.html")
+while True:
+    random_choice = random.choice(options) # escolher uma opção aleatória
+    player_choice = input("Escolha pedra, papel ou tesoura: ") # pedir ao jogador para escolher uma opção
+
+    # se o meu jogador escolher pedra
+    if player_choice == "pedra":
+        rounds_played += 1
+        if random_choice == "pedra":
+            print("Empate!")
+        elif random_choice == "papel":
+            print("Você perdeu!")
+        else:
+            print("Você ganhou!")
+            score += 1
+    
+    # se o meu jogador escolher papel
+    elif player_choice == "papel":
+        rounds_played += 1
+        if random_choice == "papel":
+            print("Empate!")
+        elif random_choice == "tesoura":
+            print("Você perdeu!")
+        else:
+            print("Você ganhou!")
+            score += 1
+
+    # se o meu jogador escolher tesoura
+    elif player_choice == "tesoura":
+        rounds_played += 1
+        if random_choice == "tesoura":
+            print("Empate!")
+        elif random_choice == "pedra":
+            print("Você perdeu!")
+        else:
+            print("Você ganhou!")
+            score += 1
+
+    # se o jogador escolher algo diferente de pedra, papel ou tesoura
+    else:
+        play_again = input("Escolha inválida! Deseja jogar novamente? (s/n): ")
+        if play_again == "s":
+            continue
+        elif play_again == "n":
+            if score == 1:
+                print(f"Você ganhou {score} rodada!", rounds_played, " rodadas jogadas.")
+            break
+        else:
+            print("Escolha inválida!")  
+            
+
